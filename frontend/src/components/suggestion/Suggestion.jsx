ab3c247bd1c1b5f172650de6c776c4c8a76e0653
Dirag Kasula <diragkasula@Dirags-MacBook-Air.local>
2026-07-08 18:40:49 +0545

    feat: update sidebar and suggestion

--- frontend/src/components/suggestion/Suggestion.jsx
+++ frontend/src/components/suggestion/Suggestion.jsx
@@ -81,23 +81,50 @@
 // export default Suggestions;
 
 import React from "react";
-import story from "../../assets/stories/stories.png";
-import story2 from "../../assets/stories/story2.jpeg";
-import story3 from "../../assets/stories/story3.jpeg";
-import story4 from "../../assets/stories/story4.jpeg";
-import story5 from "../../assets/stories/story5.png";
+import zoro from "../../assets/stories/zoro.jpeg";
+import vivi from "../../assets/stories/vivi.jpeg";
+import nami from "../../assets/stories/nami.jpeg";
+import robin from "../../assets/stories/robin.jpeg";
+import ace from "../../assets/stories/ace.webp";
+import luffy from "../../assets/stories/luffy.jpg";
 
 const Suggestions = () => {
   const suggestionsData = [
-    { id: 1, img: story, title: "Hello", desc: "this is photo", active: true },
-    { id: 2, img: story5, title: "Hello", desc: "this is photo2" },
-    { id: 3, img: story2, title: "Hello", desc: "this is photo3" },
-    { id: 4, img: story3, title: "Hello", desc: "this is photo4" },
-    { id: 5, img: story4, title: "Hello", desc: "this is photo5" },
+    { id: 1, img: luffy, title: "Luffy", desc: "I am pirate 🤴", active: true },
+    {
+      id: 2,
+      img: ace,
+      title: "Ace",
+      desc: "Igniting the seas, protecting my nakama.",
+    },
+    {
+      id: 3,
+      img: robin,
+      title: "Robin",
+      desc: "Uncovering the past, protecting the future.",
+    },
+    {
+      id: 4,
+      img: nami,
+      title: "Nami",
+      desc: "Mastering the Seas, Claiming the Treasure!",
+    },
+    {
+      id: 5,
+      img: vivi,
+      title: "Vivi",
+      desc: "Courage born of compassion; a kingdom's true shield.",
+    },
+    {
+      id: 5,
+      img: zoro,
+      title: "Zoro",
+      desc: "When I decided to follow my dream, I had already discarded my life.",
+    },
   ];
 
   return (
-    <div className="bg-white flex flex-col gap-8 w-64 p-4 rounded-2xl shadow-sm text-center">
+    <div className="bg-white flex flex-col gap-8 w-64 p-4 rounded-2xl shadow-sm text-center mt-4">
       <h1 className="font-semibold text-slate-800 text-sm">
         Suggestions For You
       </h1>
@@ -107,13 +134,13 @@ const Suggestions = () => {
           //   /* 1. Added justify-center to push the image and text block into the middle */
           <div
             key={item.id}
-            className="flex items-center justify-center gap-3 w-full"
+            className="flex items-center px-2 gap-3 w-full"
           >
             <img
               src={item.img}
               alt={item.desc}
-              className={`h-12 w-12 object-cover rounded-full ${
-                item.active ? "ring-2 ring-blue-600 ring-offset-2" : ""
+              className={`h-12 w-12 object-cover rounded-full  ${
+                item.active ? "ring-2 ring-pink-600 ring-offset-2" : "ring-2 ring-blue-600"
               }`}
             />
             {/* 2. Added items-center & text-center to stack and center the headings  */}
