1e29fbbbb388a41a71cf7325a857786fea82f0d1
Dirag Kasula <diragkasula@Dirags-MacBook-Air.local>
2026-07-08 19:23:21 +0545

    sloved story problem

--- frontend/src/components/stories/Stories.jsx
+++ frontend/src/components/stories/Stories.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import hero from "../../assets/hero.png";
-import story from "../../assets/stories/stories.png";
-import story2 from "../../assets/stories/story2.jpeg";
-import story3 from "../../assets/stories/story3.jpeg";
-import story4 from "../../assets/stories/story4.jpeg";
-import story5 from "../../assets/stories/story5.png";
+// import story from "../../assets/stories/stories.png";
+// import story2 from "../../assets/stories/story2.jpeg";
+// import story3 from "../../assets/stories/story3.jpeg";
+// import story4 from "../../assets/stories/story4.jpeg";
+// import story5 from "../../assets/stories/story5.png";
 
 const Stories = () => {
   return (
-    <div className="h-30 max-w-xl bg-blue-50 mb-3 rounded-2xl ">
+    <div className="h-30 max-w-xl bg-blue-50 mb-3 rounded- ">
       <h1 className="p-2">Stories</h1>
       <div class="flex justify-evenly items-center bg-blue-50 rounded-2xl h-25 ">
         <div className="flex flex-col items-center gap-1.5 shrink-0 p-2">
           <button className="rounded-full h-14 w-14 bg-amber-50 overflow-hidden ring-2 ring-pink-500 ring-offset-4 ring-offset-white hover:scale-105 transition-transform duration-200 focus:outline-none">
             <img
-              src={story3}
+              src={hero}
               alt="my-story"
               className="w-full h-full object-cover"
             />
@@ -26,7 +26,7 @@ const Stories = () => {
         <div className="flex flex-col items-center gap-1.5 shrink-0">
           <button className="rounded-full h-14 w-14 bg-amber-50 overflow-hidden ring-2 ring-blue-500 ring-offset-2 ring-offset-white hover:scale-105 transition-transform duration-200 focus:outline-none">
             <img
-              src={story}
+              src={hero}
               alt="story"
               className="w-full h-full object-cover"
             />
@@ -38,7 +38,7 @@ const Stories = () => {
         <div className="flex flex-col items-center gap-1.5 shrink-0">
           <button className="rounded-full h-14 w-14 bg-amber-50 overflow-hidden ring-2 ring-blue-600 ring-offset-2 ring-offset-white hover:scale-105 transition-transform duration-200 focus-outline-none">
             <img
-              src={story2}
+              src={hero}
               alt="story"
               className="w-full h-full object-cover"
             />
@@ -50,7 +50,7 @@ const Stories = () => {
         <div className="flex flex-col items-center gap-1.5 shrink-0">
           <button className="rounded-full h-14 w-14 bg-amber-50 overflow-hidden ring-2 ring-blue-600 ring-offset-2 ring-offset-white hover:scale-105 transition-transform duration-200 focus-outline-none">
             <img
-              src={story}
+              src={hero}
               alt="story"
               className="h-full w-full object-cover"
             />
@@ -62,7 +62,7 @@ const Stories = () => {
         <div className="flex flex-col items-center gap-1.5 shrink-0">
           <button className="rounded-full h-14 w-14 bg-amber-50 overflow-hidden ring-2 ring-blue-600 ring-offset-2 ring-offset-white hover:scale-105 transition-transform duration-200 focus-outline-none">
             <img
-              src={story4}
+              src={hero}
               alt="story"
               className="h-full w-full object-cover"
             />
@@ -74,7 +74,7 @@ const Stories = () => {
         <div className="flex flex-col items-center gap-1.5 shrink-0">
           <button className="rounded-full h-14 w-14 bg-amber-50 overflow-hidden ring-2 ring-blue-600 ring-offset-4 ring-offset-white hover:scale-105 transition-transform focus-outline-none">
             <img
-              src={story5}
+              src={hero}
               alt="story"
               className="h-full w-full object-cover"
             />
