a1787f8c20a663c778e09fcbc380701c2e5b9d34
sanjograiofficial <sanjograiofficial@gmail.com>
2026-07-11 11:37:24 +0545

    feat: added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,23 @@
 import express from "express";
 import routes from "./routes/routes.js";
+import cors from "cors";
 
 import "dotenv/config";
 import { globalMiddleWare } from "./middleware/error_middleware.js";
 
 const PORT = process.env.PORT;
+const frontendPort = process.env.FRONTEND_PORT;
 const app = express();
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: `http://localhost:${frontendPort}`,
+  }),
+);
 
 app.use("/", routes);
 
-app.use(globalMiddleWare)
+app.use(globalMiddleWare);
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
